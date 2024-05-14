0510c08e3098f53422caf154d094f6b652a0aa91
CitroenGames <54474809+CitroenGames@users.noreply.github.com>
2024-05-14 21:43:06 +0200

    life

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,22 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
     [Header("Attributes")]
     [SerializeField] private float m_maxHealth = 100f;
-
+    [SerializeField] private Slider slider;
     public float m_Health;
 
     private void Start()
     {
         m_Health = m_maxHealth;
+        slider.maxValue = m_maxHealth;
+        slider.value = m_Health;
     }
 
     public void TakeDamage(float damage)
     {
         m_Health -= damage;
+        slider.value = m_Health;
 
         if (m_Health <= 0)
         {
